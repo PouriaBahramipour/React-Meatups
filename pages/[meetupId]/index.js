33f9aaf444acca9e773618981bdcc8d6569111a0
pouria bahramipour <p@bahramipour.com>
2023-03-10 16:38:21 +0330

    static site generation

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/meetupDetail";
+
+const DetailsItem = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="some address 5, 12345 some city"
+      decription="this is a first meetup"
+    />
+  );
+};
+
+export default DetailsItem;
